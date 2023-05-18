36fee2b87c1d27c2b7d1ac980d2bdac16d03e671
Andrey Sukhikh <sukhikh.andrew@gmail.com>
2023-05-18 21:37:47 +0300

    Changed config for external styles

--- app/javascript/components/ReactFlow.jsx
+++ app/javascript/components/ReactFlow.jsx
@@ -8,8 +8,6 @@ import ReactFlow, {
   addEdge,
 } from 'reactflow';
 
-import 'reactflow/dist/style.css';
-
 const initialNodes = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
