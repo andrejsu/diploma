aad82ba9ef075d5ed4c58a2b6d9b99624c41effe
Andrey Sukhikh <sukhikh.andrew@gmail.com>
2023-05-21 22:02:53 +0300

    Created index page for orrganization & fixed sign_out button

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,9 +1,45 @@
 class OrganizationsController < ApplicationController
   before_action :authenticate_user!
+  before_action :load_users
   before_action :load_organization, only: %i[ show edit update destroy ]
-  before_action :load_users, only: %i[ show ]
+
+  def index
+    @organizations = Organization.all
+  end
 
   def show; end
+
+  def create
+    @organization = Organization.new(organization_params)
+
+    respond_to do |format|
+      if @organization.save
+        format.html { redirect_to device_url(@organization), notice: "Organization was successfully created." }
+        format.json { render :show, status: :created, location: @organization }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @organization.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def new
+    @organization = Device.new
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @organization.update(device_params)
+        format.html { redirect_to organization_url(@organization), notice: "Organization was successfully updated." }
+        format.json { render :show, status: :ok, location: @organization }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @organization.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root "pages#home"
+  root "organizations#index"
 
-  resources :organizations, only: [:show]
+  resources :organizations
 end
